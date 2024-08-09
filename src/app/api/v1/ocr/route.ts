ef1421b306b0b7bc586736ca9bd6ef2738da8666
junekyu02 <baekjunekyu02@gmail.com>
2024-08-09 18:31:12 +0900

    feat: llm api test2

--- src/app/api/v1/ocr/route.ts
+++ src/app/api/v1/ocr/route.ts
@@ -5,37 +5,63 @@ import OpenAI from "openai";
 
 
 export async function POST(req: Request) {
-    const { prompt } = await req.json()
+    const img = await req.blob()
 
-    const apiKey = process.env.UPSTAGE_API_KEY
+    const apiKey = process.env.UPSTAGE_API_KEY as string
     const openai = new OpenAI({
-      apiKey: apiKey,
-      baseURL: 'https://api.upstage.ai/v1/solar'
+        apiKey: apiKey,
+        baseURL: 'https://api.upstage.ai/v1/solar'
     })
-    
-    const chatCompletion = await openai.chat.completions.create({
-      model: 'solar-1-mini-chat',
-      messages: [
-        {
-            role: "system",
-            content: "Have fun conversation with user"
+
+    console.log(img)
+
+    const newFormData = new FormData()
+
+    newFormData.append("document", img)
+
+    // const formData = 
+
+    const res = await fetch("https://api.upstage.ai/v1/document-ai/ocr", {
+        method: "POST",
+        headers: {
+            // 'Content-Type': 'application/json',
+            // 'Content-Type': 'application/json',
+            // 'Content-Type': 'application/x-www-form-urlencoded',
+            
+            "Authorization": `Bearer ${apiKey}`,
+            //파일 이름만 있는 상태인데 이를 파일 데이터도 같이 보내게끔 해야함
         },
-        {
-          role: 'user',
-          content: prompt
-        }
-      ],
-    //   stream: true
-    });
-    
+        body: JSON.stringify({
+            body: newFormData
+        })
+    })
+
+    // const chatCompletion = await openai.chat.completions.create({
+    //   model: 'solar-1-mini-chat',
+    //   messages: [
+    //     {
+    //         role: "system",
+    //         content: "Have fun conversation with user"
+    //     },
+    //     {
+    //       role: 'user',
+    //       content: prompt
+    //     }
+    //   ],
+    // //   stream: true
+    // });
+
     // for await (const chunk of chatCompletion) {
     //   console.log(chunk.choices[0]?.delta?.content || '');
     // }
 
+    const testResult = await res.json()
+    // console.log(res)
+
     return NextResponse.json({
-        message: chatCompletion.choices[0].message.content
+        message: testResult
     })
-    
+
     // Use with stream=false
     // console.log(chatCompletion.choices[0].message.content || '');
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,50 +1,98 @@
 "use client"
 import Image from "next/image";
+import { useState } from "react";
 
 export default function Home() {
 
-  const testAPI = async () => {
-    const response = await fetch("/api/v1/ocr", {
-      method: "POST",
-      body: JSON.stringify({
-        prompt: "Hello"
-      })
-    })
+  const [data, setData] = useState<File | null>(null)
 
-    const data = await response.json() 
+  const testAPI = async (e: any) => {
+    // const response = await fetch("/api/v1/ocr", {
+    //   method: "POST",
+    //   body: JSON.stringify({
+    //     prompt: "Hello"
+    //   })
+    // })
 
-    console.log(data.message)
+    // const data = await response.json() 
+
+    // console.log(data.message)\
+
+    // const f = e.target.files[0]
+
+    // console.log(f)
+
+    // console.log(data)
+
 
   }
 
+  const addFile = async (event: any) => {
+
+
+    // const test = new FormData();
+    // const reader = new FileReader();
+
+    // if () {
+
+    //   const file = event.target.files[0]
+
+    //   reader.readAsDataURL(file);
+    //   console.log(file)
+    // }
+    // reader.onload = (readerEvent: any) => {
+    //   test.append("document", readerEvent.target.result);
+    // };
+
+    // console.log(test)
+
+    const res = await fetch("/api/v1/ocr", {
+      method: "POST",
+      headers: {
+        'Content-Type': 'image/jpeg',
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: event.target.files[0]
+    })
+
+    const r = await res.json()
+
+    console.log(r.message)
+  }
   return (
     <div className="container">
-    <div className="step">
-      <h2>STEP 1</h2>
-      <h2>조직 경계 문서 업로드</h2>
-      <p>조직경계 지정을 위해 사업자 등록증, 등기부등본, 임대차 계약서, 인수인계 계약서, 배출시설 설치허가증, 유해화학물질 영업허가증, 유형자산관리대장, 시설배치도, 차량관리대장 서류를 업로드 해주세요 (조직 경계에 포함된다고 생각하는 문서 제출) </p>
-      <div className="file-upload">
-        <input type="file" id="fileInput" />
-        <label htmlFor="fileInput">문서파일 업로드</label>
-        <label htmlFor="fileInput">문서를 우스로 끌어오거나, 클릭해 업로드 해주세요</label>
+      <div className="step">
+        <h2>STEP 1</h2>
+        <h2>조직 경계 문서 업로드</h2>
+        <p>조직경계 지정을 위해 사업자 등록증, 등기부등본, 임대차 계약서, 인수인계 계약서, 배출시설 설치허가증, 유해화학물질 영업허가증, 유형자산관리대장, 시설배치도, 차량관리대장 서류를 업로드 해주세요 (조직 경계에 포함된다고 생각하는 문서 제출) </p>
+        <form onSubmit={testAPI} className="file-upload">
+          <input onChange={addFile} type="file" accept="image/jpeg" id="fileInput" />
+          {data ? (
+            <Image src={URL.createObjectURL(data)} width={200} height={200} alt="NONE" />
+          ) : (
+            <>
+              <label htmlFor="fileInput">문서파일 업로드</label>
+              <label htmlFor="fileInput">문서를 우스로 끌어오거나, 클릭해 업로드 해주세요</label>
+            </>
+          )}
+        </form>
       </div>
-    </div>
 
-    <div className="step">
-      <h2>STEP 2</h2>
-      <h2>오픈뱅킹 API 연동 정보 입력</h2>
-      <p>자동 탄소배출현황 확인을 위해 정보 법인통장 내역을 확인합니다</p>
-      <div className="form-group">
-        <label htmlFor="bankApiKey">은행거래고유번호</label>
-        <input type="text" id="bankApiKey" placeholder="은행거래고유번호 (20자리)" />
+      <div className="step">
+        <h2>STEP 2</h2>
+        <h2>오픈뱅킹 API 연동 정보 입력</h2>
+        <p>자동 탄소배출현황 확인을 위해 정보 법인통장 내역을 확인합니다</p>
+        <div className="form-group">
+          <label htmlFor="bankApiKey">은행거래고유번호</label>
+          <input type="text" id="bankApiKey" placeholder="은행거래고유번호 (20자리)" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="corporateId">핀테크이용번호</label>
+          <input type="text" id="corporateId" placeholder="핀테크이용번호 (24자리)" />
+        </div>
       </div>
-      <div className="form-group">
-        <label htmlFor="corporateId">핀테크이용번호</label>
-        <input type="text" id="corporateId" placeholder="핀테크이용번호 (24자리)" />
-      </div>
-    </div>
 
-    <button onClick={testAPI} type="submit" className="submit-btn">보고서 생성하기</button>
-  </div>
+      <button onClick={testAPI} type="submit" className="submit-btn">보고서 생성하기</button>
+    </div>
   );
 }
